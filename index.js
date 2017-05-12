1d41ee148da7982d551ad9596221c1868458e7f3
Arnelle Balane <arnellebalane@gmail.com>
2017-05-12 14:51:44 +0800

    document connect and send arcus functions

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const uuid = require('uuid');
 
 const clients = {};
 
+/**
+ *  Enable Server-Sent Events (SSEs) on the given connection. All active SSE
+ *  connections are going to be kept track, identified by a uuid, so that data
+ *  can be sent to them at a later time.
+ *  @param {http.IncomingMessage} req
+ *  @param {http.ServerResponse} res
+ **/
 function connect(req, res) {
     const id = uuid();
     clients[id] = { req, res };
@@ -12,6 +19,18 @@ function connect(req, res) {
     return id;
 }
 
+/**
+ *  Send data to an SSE client identified by the given uuid.
+ *  @param {String} id - The uuid of the SSE client to send the data to.
+ *  @param {Object} options - Describes properties of the server-sent event.
+ *  @param {String} [options.event] - The name for the server-sent event.
+ *      Specifying this option will trigger the client-side EventSource's event
+ *      listeners for the event of the same name, otherwise the "message" event.
+ *  @param {String} options.data - The data that will be sent to the SSE
+ *      client, and can be obtained from "e.data" in the client-side callbacks.
+ *  Learn more about the event stream format over at this MDN page:
+ *  https://developer.mozilla.org/en-US/docs/Web/API/Server-sent_events/Using_server-sent_events#Event_stream_format
+ **/
 function send(id, options={}) {
     const res = clients[id].res;
     const data = Object.keys(options)
