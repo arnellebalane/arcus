344b1355afa5f8b184763b23835344de3fd5c912
Arnelle Balane <arnellebalane@gmail.com>
2017-05-12 16:01:18 +0800

    update arcus demo to take advantage of arcus events

--- demo/server.js
+++ demo/server.js
@@ -4,20 +4,27 @@ const morgan = require('morgan');
 const arcus = require('arcus');
 
 const app = express();
-
-app.set('views', '.');
-app.use(morgan('dev'));
-
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, './index.html')));
-
 const clients = [];
 
-app.get('/stream', (req, res) => {
-    const id = arcus.connect(req, res);
+arcus.on('connect', (id) => {
     clients.push(id);
     clients.forEach((client) => {
         arcus.send(client, { event: 'clientsupdate', data: clients.length });
     });
 });
+
+arcus.on('disconnect', (id) => {
+    const index = clients.indexOf(id);
+    clients.splice(index, 1);
+    clients.forEach((client) => {
+        arcus.send(client, { event: 'clientsupdate', data: clients.length });
+    });
+});
+
+app.set('views', '.');
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, './index.html')));
+app.get('/stream', arcus.connect);
 
 app.listen(3000);
