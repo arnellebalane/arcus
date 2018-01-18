895c330ec9eb41fe1f9a4f4b73b09ed2a8f16189
Arnelle Balane <arnellebalane@gmail.com>
2018-01-18 17:58:00 +0800

    make demo server use port from env variables

--- demo/server.js
+++ demo/server.js
@@ -27,7 +27,7 @@ app.use(morgan('dev'));
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, './index.html')));
 app.get('/stream', arcus.connect);
 
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
 
 
 
